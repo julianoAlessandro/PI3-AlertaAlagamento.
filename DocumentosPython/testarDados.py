9c586101366bd8c426b943e4d2a807e984d9d1e9
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2023-11-08 17:08:12 -0300

    Add files via upload

---
+++ DocumentosPython/testarDados.py
@@ -0,0 +1,9 @@
+#Versão:Python 3.12.0
+#Autor:Juliano Alessandro dos Santos
+#Dia: 03/11/2023
+#Biblioteca: Requests utilizada para fazer a requisição de um serviço na Web
+#refinamento do Algoritimo criando classes e metodos 
+import CadastroUsuario as c
+PrimeiroUsuario = c.CadastroUsuario('juliano','alessandro','juianoalessandro08@gmail.com',12345,'455555',89999)
+PrimeiroUsuario.realizarPrevisao(5,-22.4207671,-46.8273396)
+
