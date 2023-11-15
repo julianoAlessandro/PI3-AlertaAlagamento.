714373809ec07d12358d86d24cebe04ec1500f5b
RafaelAntunes2 <111099146+RafaelAntunes2@users.noreply.github.com>
2023-11-15 21:55:06 +0000

    git.commitMessageAccept

--- Api/sms/main.py
+++ Api/sms/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI, HTTPException, Form
+import os
+from fastapi import FastAPI, Form
 from fastapi.responses import JSONResponse
+from dotenv import load_dotenv 
 
 app = FastAPI()
 
-@app.post("/enviar-numero")
-async def enviar_numero(numero: str):    
-    # Faça algo com o número, se necessário
+@app.post("/enviar-dados")
+async def enviar_dados(numero: str = Form(...), texto: str = Form(...)):
     
-    return JSONResponse(content={"numero_recebido": numero})
+    
+    return JSONResponse(content={"numero_recebido": numero, "texto_recebido": texto})
+
+
+
+
+
+
